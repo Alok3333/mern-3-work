be7dd841c87ec1d18eb908da6c8bd34c5bf6743c
Alok3333 <alokprajapati697@gmail.com>
2024-08-16 20:08:57 +0530

    implementing on catchAsync err function

--- src/index.js
+++ src/index.js
@@ -11,15 +11,35 @@ passport.use("jwt", strategy);
 
 const router = express.Router();
 
+// create a try catch with better way
+const catchAsync = (fn) => (req, res, next) => {
+  Promise.resolve(fn, (req, res, next)).catch((err) => next(err));
+};
+
+const getResponse = () => {
+  console.log("calling getResponse function");
+};
+
 router.get(
   "/",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   (req, res) => {
     res.send({
       message: "From Controllers, Conneted with get request on '/' router",
     });
   }
 );
+
+const sampleFunction = catchAsync(async (req, res) => {
+  const awaitedResponse = await getResponse();
+  res.send(awaitedResponse);
+});
+
+router.get("/error", (req, res) => {
+  let out = sampleFunction()
+  console.log(out, "out")
+  res.end()
+});
 
 app.use("/bash", router);
 
